e8b5a26ab4810e466d577132466fba93f8afaa3d
l3miage-barryibr <95280002+l3miage-barryibr@users.noreply.github.com>
2024-01-11 20:24:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-closure
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/l3miage-barryibr/Angular-closure)
---
+++ index.ts
@@ -0,0 +1,30 @@
+function createAdder(n: number): (x: number) => number {
+  const NB = 10;
+  return (x: number) => NB + n + x;
+}
+
+const adder13 = createAdder(3);
+const adder15 = createAdder(5);
+
+console.log('_______________________');
+console.log(adder13(7));
+console.log(adder15(7));
+
+/*
+ * Définissez la fonction getFctMessage
+ *   qui prend en paramètre une chaîne de caractère str
+ *   et qui renvoie une fonction
+ *          qui ne prend pas de paramètre
+ *       et qui renvoie str.
+ * Testez votre fonction
+ *
+ */
+function getFctMessage(str: string): () => string {
+  const mess = () => str;
+  return mess;
+}
+
+const aff = getFctMessage('Bonjour');
+const resultat: string = aff();
+
+console.log(resultat);
